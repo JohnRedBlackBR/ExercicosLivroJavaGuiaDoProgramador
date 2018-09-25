23a96e25b6a818369f2451bca48dc789d005f621
John <redblack.br@outlook.com>
2018-09-25 15:49:28 -0300

    Uso da diretiva for, 1 novo programa

---
+++ Uso da diretiva for/src/uso/da/diretiva/pkgfor/UsoDaDiretivaFor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uso.da.diretiva.pkgfor;
+
+/**
+ *
+ * @author John
+ */
+public class UsoDaDiretivaFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int j;
+        //efetua a contagem de 0 a 9
+        for(j = 0 ; j < 10 ; j++){
+            System.out.println(j);
+        }
+    }
+}
